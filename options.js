d1946cc7941d04650d088c8fc4635fe1f7608752
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-12-29 10:45:02 +0530

    Initial Commit

---
+++ options.js
@@ -0,0 +1,61 @@
+export const options = {
+    autoresize: true,
+    opacity: 0.5,
+    acceleration: 1.05,
+    friction: 0.97,
+    gravity: 1.5,
+    particles: 50,
+    traceLength: 3,
+    traceSpeed: 10,
+    explosion: 5,
+    intensity: 30,
+    flickering: 50,
+    lineStyle: "round",
+    sound: {
+        enabled: true,
+        files: [
+            "./sounds/explosion0.mp3",
+            "./sounds/explosion1.mp3",
+            "./sounds/explosion2.mp3",
+        ],
+        volume: {
+            min: 6,
+            max: 25,
+        },
+    },
+    hue: {
+        min: 0,
+        max: 360,
+    },
+    delay: {
+        min: 10,
+        max: 90,
+    },
+    rocketsPoint: {
+        min: 50,
+        max: 50,
+    },
+    lineWidth: {
+        explosion: {
+            min: 1,
+            max: 3,
+        },
+        trace: {
+            min: 1,
+            max: 2,
+        },
+    },
+    brightness: {
+        min: 1,
+        max: 100,
+    },
+    decay: {
+        min: 0.001,
+        max: 0.015,
+    },
+    mouse: {
+        click: false,
+        move: false,
+        max: 1,
+    },
+};
---
+++ script.js
@@ -0,0 +1,29 @@
+import { Fireworks } from "./node_modules/fireworks-js/dist/index.es.js";
+import { options } from "./options.js";
+
+const container = document.querySelector(".container");
+const flipContainer = document.querySelector(".flipcontainer");
+const fireworks = new Fireworks(container, options);
+const title = document.createElement("h1");
+
+title.id = "title";
+
+let target = new Date(2023, 0, 1) / 1000;
+// let target = Date.now() / 1000 + 5;
+
+const flipdown = new FlipDown(target, {
+    theme: "dark",
+    headings: ["Days", "Hours", "Minutes", "Seconds"],
+});
+
+flipdown.start().ifEnded(() => {
+    flipContainer.style.transition = "1s";
+    flipContainer.style.opacity = 0;
+    setTimeout(() => {
+        // Remove the countdown and show the title
+        flipContainer.remove();
+        document.querySelector(".all").appendChild(title);
+        document.body.classList.add("newyear");
+    }, 1000);
+    fireworks.start();
+});
